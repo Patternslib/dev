f4eeb3cdbe63e63c3d7f9a79dd6f66759837a417
Johannes Raggam <thetetet@gmail.com>
2022-06-14 17:56:49 +0200

    maint: Configure jest for extendability.

--- jest.config.js
+++ jest.config.js
@@ -4,24 +4,19 @@ const path = require("path");
 // Patternslib conventions which extend this jest.config.js.
 const roots = ["./src"];
 
-// When testing in Patternslib, also add "./webpack" to roots.
-if (process.env.TEST_ENV === "patternslib") {
-    roots.push("./webpack");
-}
-
 module.exports = {
     roots: roots,
-    setupFilesAfterEnv: [path.resolve(__dirname, "./src/setupTests.js")],
+    setupFilesAfterEnv: [path.resolve(__dirname, "./jest/setup-tests.js")],
     watchPlugins: ["jest-watch-typeahead/filename", "jest-watch-typeahead/testname"],
     transform: {
         "^.+\\.[t|j]sx?$": "babel-jest",
-        "\\.(html|xml|svg)$": path.resolve(__dirname, "./webpack/jest-raw-loader.js"),
+        "\\.(html|xml|svg)$": path.resolve(__dirname, "./jest/raw-loader.js"),
     },
     moduleNameMapper: {
         "\\.(css|less|sass|scss)$": "identity-obj-proxy",
     },
     testEnvironment: "jsdom",
     transformIgnorePatterns: [
-        "/node_modules/(?!patternslib/)(?!preact/)(?!screenfull/).+\\.[t|j]sx?$",
+        "/node_modules/(?!@patternslib/)(?!preact/)(?!screenfull/).+\\.[t|j]sx?$",
     ],
 };
