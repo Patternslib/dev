259d801ab0a203ea64073e3cc0366976a38f1e0c
Johannes Raggam <thetetet@gmail.com>
2022-10-15 14:44:59 +0200

    breaking(Tests): Remove dependency on jQuery.
    Use the following in your test setup to provide jQuery to external
    libraries if they depend on a global jQuery object or extend from the
    jest config from @patternslib/patternslib:
    
    ```js
    import jquery from "jquery";
    global.$ = global.jQuery = jquery;
    ```

--- jest/setup-tests.js
+++ jest/setup-tests.js
@@ -2,15 +2,6 @@
 import "core-js/stable";
 import "regenerator-runtime/runtime";
 
-import jquery from "jquery";
-global.$ = global.jQuery = jquery;
-
-jquery.expr.pseudos.visible = function () {
-    // Fix jQuery ":visible" selector always returns false in JSDOM.
-    // https://github.com/jsdom/jsdom/issues/1048#issuecomment-401599392
-    return true;
-};
-
 // pat-fullscreen
 document.requestFullscreen = jest.fn();
 document.exitFullscreen = jest.fn();
