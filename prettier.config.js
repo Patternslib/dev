12d094badd5c743c1e59c80946035eb41c7a1950
Johannes Raggam <thetetet@gmail.com>
2024-11-07 12:39:48 +0100

    feat: Prettier: Add plugin to sort imports.

--- prettier.config.js
+++ prettier.config.js
@@ -2,4 +2,10 @@ module.exports = {
     tabWidth: 4,
     quoteProps: "consistent",
     printWidth: 89,
+    // @trivago/prettier-plugin-sort-imports
+    importOrder: ["^[./]"],
+    importOrderCaseInsensitive: false,
+    importOrderSeparation: false,
+    importOrderSortSpecifiers: true,
+    plugins: ["@trivago/prettier-plugin-sort-imports"]
 };
